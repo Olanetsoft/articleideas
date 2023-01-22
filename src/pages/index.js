384d3e9bba49d18ba3c06e526080c6818f2244c6
Kelvin Moses <moseskelvin4u@gmail.com>
2023-01-22 21:53:23 +0100

    dark mode and feature added

--- src/pages/index.js
+++ src/pages/index.js
@@ -82,67 +82,45 @@ export default function Home() {
                 <label>10 Result</label>
                 <input type="checkbox" />
               </li>
-            </ul>
-            <span className={styles.optionsWrapper}>
-              <label>Field</label>
-              <select className={styles.selectWrapper}>
-                {[
-                  { value: 'Frontend' },
-                  { value: 'Backend' },
-                  { value: 'Cloud' },
-                  { value: 'UI/UX' },
-                  { value: 'Web3' },
-                ].map((item) => (
-                  <option value={item} key={item}>
-                    {item.value}
+              <span className={styles.optionsWrapper}>
+                <label>Field</label>
+                <select className={styles.selectWrapper}>
+                  {[
+                    { value: 'Frontend' },
+                    { value: 'Backend' },
+                    { value: 'Cloud' },
+                    { value: 'UI/UX' },
+                    { value: 'Web3' },
+                  ].map((item) => (
+                    <option value={item} key={item}>
+                      {item.value}
+                    </option>
+                  ))}
+                  <option value="" key="" className={styles.selectWrapper}>
+                    Frontend
                   </option>
-                ))}
-                <option value="" key="" className={styles.selectWrapper}>
-                  Frontend
-                </option>
-              </select>
-            </span>
+                </select>
+              </span>
+            </ul>
           </div>
 
           <div style={{ marginTop: '2rem' }}>
             <div class="flex flex-col">
               <div class="overflow-x-auto sm:mx-0.5 lg:mx-0.5">
-                <div class="py-2 inline-block min-w-full sm:px-6 lg:px-8">
+                <div class="py-1 inline-block min-w-full sm:px-6 lg:px-8">
                   <div class="overflow-hidden">
                     <div class="w-4/5 mt-5" style={{ margin: '0 auto' }}>
                       {' '}
-                      <thead class="bg-white border-b">
-                        <tr>
-                          <th
-                            scope="col"
-                            class="text-sm font-medium text-gray-900 px-6 py-4 text-left"
-                          >
-                            Generated Topics
-                          </th>
-                        </tr>
-                      </thead>
-                      <ol
-                        class="bg-gray-100 border-b"
-                        style={{ listStyle: 'none' }}
-                      >
+                      <ol class="bg-[rgb(220,220,220)] border-b">
                         {[{}].map((item) => (
                           <>
-                            <li class="bg-gray-100 border-b">
-                              <span class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap font-medium">
-                                <a href="#" style={{ color: 'blue' }}>
-                                  Easily Create An NFT App Using The New Infura
-                                  NFT SDK TypeScript
-                                </a>
-                              </span>
+                            <li
+                              scope="col"
+                              class="text-sm font-medium text-gray-900 px-6 py-2 text-left"
+                            >
+                              Get Started with DAOs: A Step-by-Step Guide Using
+                              Boardroom
                             </li>
-                            <tr class="bg-gray-100 border-b">
-                              <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap font-medium">
-                                <a href="#" style={{ color: 'blue' }}>
-                                  Easily Create An NFT App Using The New Infura
-                                  NFT SDK TypeScript
-                                </a>
-                              </td>
-                            </tr>
                           </>
                         ))}
                       </ol>
